85dd3e1f93a2df0832e7bc22744dbb920637a07d
bakasoft <40889043+bakasoftcom@users.noreply.github.com>
2018-09-07 09:22:00 +0430

    First version
    Structure

---
+++ salmple1.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        #main loop
+
+
+
+
+
+        #end of display functions
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
